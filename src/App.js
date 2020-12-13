fc3afc7afa56d49523c995fecbeeb53e57366800
usoni11 <u.soni@onefc.com>
2020-12-13 21:56:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+import SearchBox from './SearchBox.js';
+
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [inputValue, setValue] = useState();
+
+  const search = (e) => {
+    setValue(e.target.value);
+    let results = [];
+    fetch('http://www.mocky.io/v2/5ba8efb23100007200c2750c')
+    .then(response => response.json())
+    .then(data =>{
+
+      for(var i=0; i<data.length; i++) {
+        let found=0;
+        for(const key in data[i]) {
+          if(found===1) {
+            continue;
+          }
+          let text = data[i][key];
+          if(typeof(text) === 'object') {
+            if(text.indexOf(e.target.value.toLowerCase())!==-1) {
+              results.push(data[i]);
+              found=1;
+            }
+          } else {
+            if(text.toLowerCase().indexOf(e.target.value.toLowerCase())!==-1) {
+              results.push(data[i]);
+              found=1;
+            }
+          }
+          
+        }
+      }
+      setSearchResults(results);
+    });
+  };
+
+  return (
+    <div className="App">
+      <SearchBox 
+      handleOnChange={search} 
+      searchResults={searchResults} 
+      inputValue={inputValue}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,103 @@
+import Parser from 'html-react-parser';
+import { useEffect, useRef, useState } from 'react';
+
+const Autocomplete = ({handleOnChange, searchResults, inputValue}) => {
+    const [focus, setFocus] = useState(null);
+    const inputRef = useRef();
+    const itemsRef = useRef([]);
+
+    useEffect(() => {
+        itemsRef.current = itemsRef.current.slice(0, searchResults.length);
+     }, [searchResults]);
+
+    const renderHighlighted = (text) => {
+        const index = text.toLowerCase().indexOf(inputValue.toLowerCase());
+        if(index > -1) {
+            const result = text.substring(0, index)+'<b>'+text.substring(index, index+inputValue.length)+'</b>'+text.substring(index+inputValue.length);
+            return result;
+        }
+        return text;
+    };
+
+    const handleKeyDown = (e) => {
+        if(e.keyCode === 40) {
+            if(e.target.type === "text") {
+                    itemsRef.current[0].focus();
+                    setFocus(0);
+                
+            } else {
+                if(focus < searchResults.length) {
+                    setFocus(focus+1);
+                    itemsRef.current[focus+1].focus();
+                }
+                
+            }
+            
+        } else if(e.keyCode === 38) {
+            if(focus !== null && focus !== 0) {
+                setFocus(focus-1);
+                itemsRef.current[focus-1].focus();
+            }
+        } else if (e.keyCode === 27) {
+            inputRef.current.focus();
+            setFocus(null);
+        } else if (e.keyCode === 13) {
+            handleOnChange({target: {value: searchResults[focus].name}});
+            inputRef.current.focus();
+            setFocus(null);
+        }
+    }
+
+    const handleMouseOver = (i) => {
+        itemsRef.current[i].focus();
+        setFocus(i);
+    }
+
+    return (
+    <div className="search-box">
+        <input type="text"
+        ref={inputRef}
+        onChange={handleOnChange} 
+        className="search-input" 
+        placeholder="Search users by id, address, name, pincode"
+        onKeyDown={handleKeyDown}
+        value={inputValue || ''}
+        />
+
+        <div className="options" >
+            {inputValue && (searchResults.length ?
+            
+                ( searchResults.map((result, i) => {
+                    return (
+                        <div tabIndex={i}
+                            onMouseOver={()=>{handleMouseOver(i)}}
+                            ref={el => itemsRef.current[i] = el} 
+                            value={i}
+                            onKeyDown={handleKeyDown} 
+                            className="option-item" key={i}
+                            onClick={()=>handleOnChange({target: {value: searchResults[i].name}})}
+                        >
+                            <br></br>
+                            <strong>{result.id}</strong>
+                            <br></br>
+                            <i>{Parser(renderHighlighted(result.name))}</i>
+                            <br></br>
+                            <p className="address">{Parser(renderHighlighted(result.address))}</p>
+                        </div>
+                    )
+                }) )
+             : (
+             
+                <div className="option-item">
+                    <p>No user found</p>
+                </div>
+            
+             ))
+             }
+            
+        </div>
+    </div>);
+}
+
+
+export default Autocomplete;
